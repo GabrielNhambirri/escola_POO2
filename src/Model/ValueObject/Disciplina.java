16ae50e5a76f7e488c4e2ac7906879643a043088
isac <grojisac@gmail.com>
2021-12-02 01:22:04 +0200

    Commit

---
+++ src/Model/ValueObject/Disciplina.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objectos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author isacl
+ */
+public class Disciplina implements Serializable{
+    private int idDisciplina;
+    private String nome;
+
+    public Disciplina(int idDisciplina, String nome) {
+        this.idDisciplina = idDisciplina;
+        this.nome = nome;
+    }
+
+    public int getIdDisciplina() {
+        return idDisciplina;
+    }
+
+    public void setIdDisciplina(int idDisciplina) {
+        this.idDisciplina = idDisciplina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String toString(){
+        String format = "%-20s %-20s";
+        return String.format(format, idDisciplina, nome);
+    }
+}
---
+++ src/Model/ValueObject/Professor.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objectos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author isacl
+ */
+public class Professor extends Pessoa implements Serializable{
+
+    private String grauAcademico;
+
+    public Professor(Pessoa x, String grauAcademico){
+        super(x);
+        this.grauAcademico = grauAcademico;
+    }
+    
+    public void update(Pessoa x, String grauAcademico){
+        update(x);
+        this.grauAcademico = grauAcademico;
+    }
+
+    public String getGrauAcademico() {
+        return grauAcademico;
+    }
+
+    public String toString() {
+        String format = "%-20s";
+        return super.toString() + String.format(format, grauAcademico);
+    }
+
+}
