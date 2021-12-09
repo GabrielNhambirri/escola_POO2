d2acac2e9d922cced503660363ca54bcaf03cbf9
GabrielNhambirri <95540503+GabrielNhambirri@users.noreply.github.com>
2021-12-09 21:49:43 +0200

    Add files via upload

---
+++ src/View/interface_disciplina.java
@@ -0,0 +1,17 @@
+
+package interface_disciplina;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class interface_disciplina {
+    
+    public static void main(String[] args) {
+        
+        Disciplina disciplina = new Disciplina();
+        
+        
+    }
+    
+}
