84a8832f69c46d4022221c7022ac638e0943df9a
GabrielNhambirri <95540503+GabrielNhambirri@users.noreply.github.com>
2021-12-09 21:47:58 +0200

    Add files via upload

---
+++ src/View/Interface_Professor.java
@@ -0,0 +1,18 @@
+
+package interface_professor;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Interface_Professor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Professor pf = new Professor();
+
+    }
+    
+}
